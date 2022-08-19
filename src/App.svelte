514eb537800c20026d4aa50552b6ad9d0ac4b5ac
sato_tats <satotats.alive@gmail.com>
2022-08-19 17:05:50 +0900

    initial hideyuki

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Slot from "./lib/Slot.svelte";
+</script>
+
+<main>
+  <h3>Super Hideyuki Generator v1.0</h3>
+
+  <div class="card">
+    <Slot />
+  </div>
+</main>
---
+++ src/lib/Slot.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  let hide: string = "ひで";
+  let yuki: string = "ゆき";
+  let hideyuki: string = undefined;
+  const genHideyuki = () => {
+    hide = genKanji(HIDES);
+    yuki = genKanji(YUKIS);
+    hideyuki = hide + yuki;
+  };
+
+  const genKanji = (dataset: string[]) => {
+    return dataset[random(dataset.length)];
+  };
+
+  const random = (i: number) => {
+    return Math.floor(Math.random() * i);
+  };
+
+  const HIDES = [
+    "英",
+    "秀",
+    "英",
+    "日出",
+    "偉",
+    "映",
+    "栄",
+    "任",
+    "継",
+    "HIDE",
+  ];
+  const YUKIS = [
+    "行",
+    "幸",
+    "征",
+    "之",
+    "享",
+    "淑",
+    "豪",
+    "侑",
+    "如",
+    "志",
+    "往",
+    "維",
+    "雪",
+    "将",
+    "秀",
+    "介",
+    "由紀",
+    "礼",
+    "敬",
+    "鵬",
+    "進",
+    "享",
+    "YUKI",
+  ];
+</script>
+
+{#if hideyuki}
+  <p>
+    今日から君は <b>"{hideyuki}" (ひでゆき)</b> だ！！
+  </p>
+{/if}
+<button on:click={genHideyuki}> 漢字を得る </button>
+
+{#if hideyuki}
+  <p>
+    Search the meaning of&nbsp;<a
+      href="https://www.google.com/search?q={hide}"
+      target="_blank">{hide}</a
+    >
+  </p>
+  <p>
+    Search the meaning of&nbsp;<a
+      href="https://www.google.com/search?q={yuki}"
+      target="_blank">{yuki}</a
+    >
+  </p>
+{/if}
+
+<style>
+  p b {
+    font-size: 200%;
+  }
+</style>
